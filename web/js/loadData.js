f6ac4be7df64623bcc89917455781226c6f64092
Nicolas Rod <ncls.rod@gmail.com>
2017-10-12 00:10:47 +0200

    Preparation for github.io

--- web/js/loadData.js
+++
@@ -1,9 +0,0 @@
-function fillData() {
-  $.getJSON('data/data.json', (jsonData) => {
-    $('#commitStats').text(`
-          You have made YY commits, adding XX lines, deleted XX, for a total of XXX. That's an average of XX lines modified per commit.
-        `);
-  });
-}
-
-fillData();
