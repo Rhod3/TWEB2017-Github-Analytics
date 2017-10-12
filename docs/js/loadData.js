4192f456d86e85a1d36d562f4c41852cae43bef1
Nicolas Rod <ncls.rod@gmail.com>
2017-10-12 08:52:42 +0200

    Data visualisation test

--- docs/js/loadData.js
+++ docs/js/loadData.js
@@ -1,7 +1,8 @@
 function fillData() {
-  $.getJSON('data/data.json', (jsonData) => {
+  const currentUser = "Rhod3";
+  $.getJSON('data/data.json', (json) => {
     $('#commitStats').text(`
-          You have made YY commits, adding XX lines, deleted XX, for a total of XXX. That's an average of XX lines modified per commit.
+          You have made ${json[currentUser].statsGlobal.nbCommit} commits, adding ${json[currentUser].statsGlobal.nbAdd} lines, deleted ${json[currentUser].statsGlobal.nbDelete}, for a total of ${json[currentUser].statsGlobal.nbTotal}. <br/><br/> That's an average of ${json[currentUser].statsGlobal.nbTotalPerCommit} lines modified per commit.
         `);
   });
 }
