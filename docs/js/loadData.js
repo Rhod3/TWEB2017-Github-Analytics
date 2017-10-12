b35876bbb7a7161321cfb9a1b043de5b183ad57d
Nicolas Rod <ncls.rod@gmail.com>
2017-10-12 10:00:33 +0200

    Visualisation v4

--- docs/js/loadData.js
+++ docs/js/loadData.js
@@ -2,12 +2,12 @@ function fillData() {
   const currentUser = "Rhod3";
   $.getJSON('data/data.json', (json) => {
     $('#commitStats').text(`
-      You have made ${json[currentUser].statsGlobal.nbCommit} commits, adding ${json[currentUser].statsGlobal.nbAdd} lines, deleted ${json[currentUser].statsGlobal.nbDelete}, for a total of ${json[currentUser].statsGlobal.nbTotal}. \n\n 
+      You have made ${json[currentUser].statsGlobal.nbCommits} commits, adding ${json[currentUser].statsGlobal.nbAdd} lines, deleted ${json[currentUser].statsGlobal.nbDelete}, for a total of ${json[currentUser].statsGlobal.nbTotal}. \n\n 
       That's an average of ${json[currentUser].statsGlobal.nbTotalPerCommit} lines modified per commit.
     `);
 
     $('#messageStats').text(`
-    You have made ${json[currentUser].statsGlobal.nbCommit} commits, containing a total of ${json[currentUser].statsGlobal.nbWordsMessage} words of commit message. \n\n
+    You have made ${json[currentUser].statsGlobal.nbCommits} commits, containing a total of ${json[currentUser].statsGlobal.nbWordsMessage} words of commit message. \n\n
     That's an average of ${json[currentUser].statsGlobal.nbWordsMessagePerCommit} words of message per commit.
     `);
   });
