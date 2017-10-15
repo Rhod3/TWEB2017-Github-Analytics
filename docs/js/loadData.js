36f29f3ff1b965ded3b9c1429f67876504c6465a
Nicolas Rod <ncls.rod@gmail.com>
2017-10-15 17:27:27 +0200

    User select and chart WIP5

--- docs/js/loadData.js
+++ docs/js/loadData.js
@@ -22,6 +22,27 @@ function fillData() {
       That's an average of ${json[currentUser].statsGlobal.nbWordsMessagePerCommit} words of message per commit.
     `);
   });
+
+  // Create graph with chart.js
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+  
+      // The data for our dataset
+      data: {
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [{
+              label: "My First dataset",
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45],
+          }]
+      },
+  
+      // Configuration options go here
+      options: {}
+  });
 }
 
 fillData();
