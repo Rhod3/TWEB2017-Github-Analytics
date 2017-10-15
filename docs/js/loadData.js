da171a73ff44295d59ff83904bb5d87ff5cfc408
Nicolas Rod <ncls.rod@gmail.com>
2017-10-15 16:55:34 +0200

    Added select creation

--- docs/js/loadData.js
+++ docs/js/loadData.js
@@ -1,6 +1,14 @@
 function fillData() {
   const currentUser = "Rhod3";
   $.getJSON('data/data.json', (json) => {
+
+    $.each(json, function (i, item) {
+      $('#userSelect').append($('<option>', { 
+        value: item.value,
+        text : item.text 
+      }));
+    });
+
     $('#commitStats').text(`
       You have made ${json[currentUser].statsGlobal.nbCommits} commits, adding ${json[currentUser].statsGlobal.nbAdd} lines, deleted ${json[currentUser].statsGlobal.nbDelete}, for a total of ${json[currentUser].statsGlobal.nbTotal}. \n\n 
       That's an average of ${json[currentUser].statsGlobal.nbTotalPerCommit} lines modified per commit.
