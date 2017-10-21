606170e308f56286652250ac900803b7ee152e44
Nicolas Rod <ncls.rod@gmail.com>
2017-10-21 02:38:14 +0200

    Fixes word cloud

--- docs/js/loadData.js
+++ docs/js/loadData.js
@@ -69,30 +69,50 @@ function fillData(currentUser) {
         }
     });
 
+    console.log(json[currentUser].statsGlobal.messages);
+    
     var myConfig = {
-      "graphset":[
-      {
-      "type":"wordcloud",
-      "options": {
-        "style": {
-          "tooltip": {
+      type: 'wordcloud',
+      options: {
+        text: json[currentUser].statsGlobal.messages,
+        minLength: 5,
+        ignore: [],
+        maxItems: 40,
+        aspect: 'flow-center',
+        rotate: true,
+        colorType: 'palette',
+        palette: ['#D32F2F','#5D4037','#1976D2','#E53935','#6D4C41','#1E88E5','#F44336','#795548','#2196F3','#EF5350','#8D6E63','#42A5F5'],
+        
+        style: {
+          fontFamily: 'Crete Round',
+          
+          hoverState: {
+            backgroundColor: '#D32F2F',
+            borderRadius: 2,
+            fontColor: 'white'
+          },
+          tooltip: {
+            text: '%text: %hits',
             visible: true,
-            text: '%text: %hits'
+            
+            alpha: 0.9,
+            backgroundColor: '#1976D2',
+            borderRadius: 2,
+            borderColor: 'none',
+            fontColor: 'white',
+            fontFamily: 'Georgia',
+            textAlpha: 1
           }
-        },
-      "words": json[currentUser].statsGlobal.messages
-      }
-      }
-    ]
+        }
+      },
     };
-     
+    
     zingchart.render({ 
       id: 'cloudWordChart', 
       data: myConfig, 
-      height: '100%', 
+      height: 400, 
       width: '100%' 
     });
-
   });
 }
 
